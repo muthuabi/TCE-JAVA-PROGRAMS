a0b7f07014f7260a5dba6ab9351ac0f634387766
muthuabi <muthuabi292@gmail.com>
2025-03-21 10:28:59 +0530

    Exercise 7 verified

--- lab_ex7/TrafficManagementSystem.java
+++ lab_ex7/TrafficManagementSystem.java
@@ -1,17 +1,37 @@
 package lab_ex7;
 import java.util.Scanner;
 
-class TrafficSignal {
+class TrafficSignal implements Runnable{
     String signalId;
     private String status;
-
+    int index=0;
+    String signals[]=new String[] {"Red","Green","Yellow"};
+    Thread t=new Thread(this,"Traffic");
     public TrafficSignal(String signalId) {
         this.signalId = signalId;
         this.status = "Red"; 
     }
-
+    public void run()
+    {
+    	while(true)
+    	{
+    		try {
+    	
+    	System.out.println("Signal:"+signalId+" Status:"+signals[index]);
+    	System.out.println();
+    	index=(index+1)%3;
+    	Thread.sleep(3000);
+    		}
+    		catch(InterruptedException  ie)
+    		{
+    			
+    		}
+    	}
+    	
+    }
     public void setStatus(String status) {
         this.status = status;
+        this.index=(signals[0].equalsIgnoreCase(status)?0:(signals[1].equalsIgnoreCase(status)?1:2));
     }
 
     public void showSignal() {
@@ -56,7 +76,8 @@ public class TrafficManagementSystem {
         j1.showJunctionSignals();
         j1.setSignalStatus(sc);
         j1.showJunctionSignals();
-
+        s1.t.start();
+        s2.t.start();
         sc.close();
     }
 }
